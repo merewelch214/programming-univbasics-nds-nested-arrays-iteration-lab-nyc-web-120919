54a4d3572ff55e03428a405a964407d4b9ef5722
comical-sandbox-7108 <merewelch@gmail.com>
2019-10-30 12:06:15 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,6 +49,7 @@ def total_even_pairs(src)
     if (src[row_index][0] % 2 == 0) && src[row_index][1] % 2 == 0)
       total += src[row_index][0]
       total += src[row_index][1]
+    end
     row_index += 1
   end
   return total
